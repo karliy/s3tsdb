48ec54451fa806d6a50ae87c3d3cbaa0adc46c38
karfly <karfly@qq.com>
2018-09-19 10:24:04 +0800

    upgrade

--- test.go
+++ test.go
@@ -1,34 +1,46 @@
 package main
+
 import (
+	"fmt"
+	"time"
+    "strconv"
     "net/http"
     "net/url"
+    "math/rand"
     "io/ioutil"
-    "fmt"
-    "runtime"
 )
+
 func Use(vals ...interface{}) {
     for _, val := range vals {
         _ = val
     }
 }
 
-func tests3tsdb() {
+func tests3tsdb(host string) {
     resp, err := http.PostForm("http://127.0.0.1:8080/push",
-	url.Values{"ts": {"1531261558"}, "metric": {"cpu.load"}, "host": {"elk"}, "value": {"5.1"}})
+    url.Values{"ts": {strconv.FormatInt(time.Now().Unix(), 10)}, "metric": {"cpu.load"}, "host": {host}, "value": {strconv.FormatFloat(rand.Float64(), 'f', 6, 64)}})
     if err != nil {
         fmt.Printf("http error %s", err)
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
-    fmt.Println(string(body))
-    runtime.Gosched()
+    if err != nil {
+        fmt.Printf("http error %s", err)
+    }
+    Use(body)
+
+}
+
+func say(host string) {
+	for {
+		time.Sleep(time.Second)
+        tests3tsdb(host)
+	}
 }
 
 func main() {
-    for a := 0; a < 10; a++ {
-        go tests3tsdb()
+    for i :=1; i < 799; i++ {
+        go say("elk" + strconv.Itoa(i))
     }
+	say("elk1000")
 }
-
-
-
